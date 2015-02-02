436e387231697b10e5baf6a2404d00d1fd3e1f45
Matt Linaberry <matt.linaberry@we-ics.com>
2015-02-02 17:18:58 -0500

    Added assets and Slot struct

---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,16 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Matthew Linaberry on 2/2/15.
+//  Copyright (c) 2015 Matthew Linaberry. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image = UIImage(named: "Ace")
+    var isRed = true
+}
